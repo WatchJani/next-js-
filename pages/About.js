67bdbfb279cf96d66471424b49a03645608347a9
WatchJani <jankokondic84@gmail.com>
2022-07-13 16:58:48 +0200

    slike

--- pages/About.js
+++ pages/About.js
@@ -1,30 +1,39 @@
 import React from 'react'
 import Link from "next/link"
-
+import Image from 'next/image'
 
 
 export async function getServerSideProps() {
-    const res = await fetch("https://jsonplaceholder.typicode.com/todos")
+    const res = await fetch("http://localhost:5000/api/v1/all")
     const data = await res.json()
 
     return {
         props: {
-            todose: data
+            movies: data.movies
         }
     }
 }
 
-const About = ({todose}) => {
+const About = ({ movies }) => {
+
+    console.log(movies)
+
     return (
         <>
             <Link href="/">home</Link>
             <div>About</div>
-            {todose?.length == 0 ? (<div>loading...</div>) : (todose.map((todo) => {
+            {movies?.length == 0 ? (<div>loading...</div>) : (movies.map((movies) => {
                 return (
-                    <div key={todo.id}>
-                        <p>
-                            {todo.id}: {todo.title}
-                        </p>
+                    <div key={movies._id}>
+                        <Image
+                            src={movies.banner}
+                            alt={movies.name}
+                            priority
+                            width={1280}
+                            height={810}
+                        />
+                        <h1>{movies.name}</h1>
+                        <p>{movies.description}</p>
                     </div>
                 )
             }))
